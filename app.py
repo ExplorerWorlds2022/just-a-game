a6717a19bb751b146a6015d072c40105b8ae561b
ExplorerWorlds2022 <pavel.gorinov02@gmail.com>
2024-10-08 18:50:09 +0300

    80%

--- app.py
+++ app.py
@@ -1,62 +1,72 @@
-field = list()
-
-
-def start_game():
-    response = input("Желаете начать игру? (Да/Нет) ")
-    if response == "Да":
-        print('''Вводите номер строки и столбца клетки, чтобы поставить туда крестик/нолик. 
-            Побеждает игрок, поставивший свои символы в одну линию. Приятной игры!''')
-        game()
-    elif response == "Нет":
-        print("Мудрое решение!")
-    else:
-        print("Некорректный ввод, попробуйте ещё раз!")
-        start_game()
+field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]    
 
 
 def game():
     global field
-    line, column = list("-------"), list("| | | |")
-    field = [line, column, line, column, line, column, line]
-    player_turn = 0
-    while check_win() == False:
+
+    player_turn, x, y = 0, 0, 0
+    for turn in range(9):
         draw_field()
-        x, y = map(int, input(f"{player_turn + 1} игрок ходит: ").split())
-        field[x][y] = "X" if player_turn == 0 else "O"
-        player_turn = (player_turn + 1) % 2
-    print(f'Победил {p}')
+        player_turn = turn % 2 + 1
+        print(f"{player_turn} игрок ходит:")
+        try:
+            x, y = map(int, input().split())
+        except:       
+            while check_input(x, y) == False:
+                print('Неверный ввод данных! Введите координаты поля в формате "2 2"')
+                x, y = map(int, input().split())
+
+        field[x - 1][y - 1] = "X" if player_turn == 1 else "O"
+        if check_win():
+            break
+            
+    if check_win():
+        print(f'Победил {player_turn} игрок!')
+        draw_field()
+    else:
+        print('Ничья!')
 
 
 def draw_field():
     for line in field:
-        print(*line)
-
+        print(f'-------\n|{line[0]}|{line[1]}|{line[2]}|')
+        
+    print('-------')
 
 def check_win():
-    for line_num in range(1, 7, 2):
-        line = field[line_num]
-        if line.count("X") == 3:
-            return 1
-        elif line.count("O") == 3:
-            return 2
+    for line in field:
+        if line.count("X") == 3 or line.count("O") == 3:
+            return True
 
-    for column in range(1, 7, 2):
-        if field[1][column] == field[3][column] == field[5][column] == "X":
-            return 1
-        elif field[1][column] == field[3][column] == field[5][column] == "O":
-            return 2
+    for column in range(3):
+        if field[0][column] == field[1][column] == field[2][column] == "X" or \
+            field[0][column] == field[1][column] == field[2][column] == "O":
+            return True
         
-    if line[1][1] == line[3][3] == line[5][5] == 'X' or \
-    line[1][5] == line[3][3] == line[5][1] == 'X':
-        return 1
-    elif line[1][1] == line[3][3] == line[5][5] == 'O' or \
-    line[1][5] == line[3][3] == line[5][1] == 'O':
-        return 2
+    if field[0][0] == field[1][1] == field[2][2] == 'X' or \
+    field[0][0] == field[1][1] == field[2][2] == 'X' or \
+    field[0][2] == field[1][1] == field[2][0] == 'O' or \
+    field[0][2] == field[1][1] == field[2][0] == 'O':
+        return True
     
+    return False
+
+
+def check_input(x, y):
+    if type(x) == type(y) == int and 1 <= x <= 3 and 1 <= y <= 3 and field[x-1][y-1] == ' ':
+        return True
     return False
 
 print(
     '''Хотите убить немного времени с приятелем? Вы по адресу!
-Добро пожаловать в "Крестики Нолики!"'''
+    Добро пожаловать в "Крестики Нолики!"'''
 )
-start_game()
+
+response = input("Желаете начать игру? (Да/Нет) ")
+if response == "Да":
+    print('''Правила: вводите вначале номер строки, а затем номер столбца клетки, чтобы поставить туда крестик/нолик в формате "2 2". \nПобеждает игрок, поставивший свои символы в одну линию. Приятной игры!''')
+    game()
+elif response == "Нет":
+    print("Мудрое решение!")
+else:
+    print("Некорректный ввод, попробуйте ещё раз!")
